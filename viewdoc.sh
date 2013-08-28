5c5a2c18fa914e9c8e9bc7d66299e5b176bdc655
Brian Kubisiak <bkubisiak.git@gmail.com>
2013-08-27 23:21:38 -0500

    Added support for ps documents

--- viewdoc.sh
+++ viewdoc.sh
@@ -3,8 +3,8 @@
 DIR="$HOME/library"
 VIEWER="zathura"
 
-TITLE=`ls $DIR | grep -e .pdf -e .djvu | sed 's/_/\ /g;s/.pdf//g;s/.djvu//g' | dmenu -i | sed 's/\ /_/g'`
+TITLE=`ls $DIR | grep -e .pdf -e .djvu -e .ps | sed 's/_/\ /g;s/.pdf//g;s/.djvu//g;s/.ps//g' | dmenu -i | sed 's/\ /_/g'`
 
-[ "$TITLE" ] && FILENAME=`ls $DIR | grep -w -e $TITLE.pdf -e $TITLE.djvu`
+[ "$TITLE" ] && FILENAME=`ls $DIR | grep -w -e $TITLE.pdf -e $TITLE.djvu -e $TITLE.ps`
 
 [ -f $DIR/$FILENAME ] && $VIEWER $DIR/$FILENAME &
