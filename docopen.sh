14232082dc5bd0390b18ee7445a28beb9fc57b5f
Brian Kubisiak <bkubisiak.git@gmail.com>
2014-03-12 19:54:59 -0700

    Added document opener

---
+++ docopen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+VIEWER="zathura"
+
+CUR="$HOME"
+
+while [ $CUR ] && [ -d $CUR ]
+do
+    cd $CUR
+    CUR=`echo "$(ls | grep -e "\.pdf" -e "\.djvu" -e "\.ps")
+$(ls -d */)" | sed '/^$/d' | sort | dmenu`
+done
+
+if [ $CUR ] && [ -f $CUR ]
+then
+    $VIEWER $CUR
+fi
+
