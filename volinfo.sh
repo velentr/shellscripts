0c287df052f36d84e972fc8cd890541f4e7f4dda
Brian Kubisiak <velentr.rc@gmail.com>
2015-03-16 13:30:43 -0700

    Small modifications.

--- volinfo.sh
+++ volinfo.sh
@@ -5,5 +5,5 @@ volume=`amixer get Master | grep Mono:`
 if [ "$(echo $volume | grep off)" ]; then
     echo Mute
 else
-    echo `echo $volume | cut -f4 -d' ' | sed 's/\[//g;s/\]//g'`
+    echo `echo $volume | cut -f4 -d' ' | sed -e 's/%//g' -e 's/\[//g;s/\]//g'`
 fi
